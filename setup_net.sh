e3bbf0018cd2b771fdcc61b158c6839c69d1b641
Adam Walker <adamwalker10@gmail.com>
2019-09-27 22:47:51 +1000

    net setup script

---
+++ setup_net.sh
@@ -0,0 +1,7 @@
+#!/bin/sh 
+# Enable the interface connected to the Arty
+ip link set dev $1 up
+# Give that interface an IP address
+ip addr add 192.168.5.1/24 dev $1
+# Give the Arty a dummy MAC so it doesn't need to respond to ARPs
+ip neigh add 192.168.5.2 lladdr 00:80:77:31:01:07 nud permanent dev $1
