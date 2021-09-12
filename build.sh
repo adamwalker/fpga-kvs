22e0b034cc70f2dd42338048bc4c67fcc0a5e0fb
Adam Walker <adamwalker10@gmail.com>
2021-09-12 21:04:26 +1000

    update for clash 1.4.3

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 pushd clash
 stack exec clash -- --verilog kvs.hs
 popd
-cp clash/verilog/Main/kvs/* fpga/src/
+cp clash/verilog/Main.topEntity/* fpga/src/
 pushd fpga
 vivado -mode batch -nojournal -source compile.tcl
 
